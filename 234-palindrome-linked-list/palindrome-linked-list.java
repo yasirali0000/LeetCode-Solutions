82eb0991a6cfa518837889c7b701cd7b55b8dfba
yasirali0000 <yasihkhan33@gmail.com>
2026-08-22 13:24:37 +0530

    Time: 4 ms (66.97%) | Memory: 94.3 MB (79.83%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.java
+++ 234-palindrome-linked-list/palindrome-linked-list.java
@@ -9,21 +9,52 @@
  * }
  */
 class Solution {
-    public boolean isPalindrome(ListNode head) {
-        ListNode temp = head;
-        ArrayList<Integer> arr = new ArrayList<>();
-        while(temp!=null) {
-            arr.add(temp.val);
-            temp = temp.next;
+    public static ListNode reverse(ListNode head) {
+        ListNode curr = head;
+        ListNode prev = null;
+        ListNode fwd = null;
+        while(curr!=null) {
+            fwd = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = fwd;
         }
-        int i=0,j=arr.size()-1;
-        while(i<=j) {
-            int a = arr.get(i);
-            int b = arr.get(j);
-            if(a!=b) return false;
-            i++;
-            j--;
+        return prev;
+    }
+    public boolean isPalindrome(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast.next!=null && fast.next.next!=null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        ListNode head2 = slow.next;
+        slow.next = null;
+        head2 = reverse(head2);
+        ListNode i = head;
+        ListNode j = head2;
+        while(j!=null) {
+            if(i.val!=j.val) return false;
+            i = i.next;
+            j = j.next;
         }
         return true;
+
+
+        // ListNode temp = head;
+        // ArrayList<Integer> arr = new ArrayList<>();
+        // while(temp!=null) {
+        //     arr.add(temp.val);
+        //     temp = temp.next;
+        // }
+        // int i=0,j=arr.size()-1;
+        // while(i<=j) {
+        //     int a = arr.get(i);
+        //     int b = arr.get(j);
+        //     if(a!=b) return false;
+        //     i++;
+        //     j--;
+        // }
+        // return true;
     }
 }
