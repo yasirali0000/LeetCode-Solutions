7e944d6932c159dd501916514c651442091e964d
yasirali0000 <yasihkhan33@gmail.com>
2026-08-26 21:44:26 +0530

    Time: 30 ms (90.09%) | Memory: 102.2 MB (26.39%) - LeetSync

--- 155-min-stack/min-stack.java
+++ 155-min-stack/min-stack.java
@@ -1,27 +1,56 @@
 class MinStack {
-    Stack<Integer> st;
-    Stack<Integer> minst;
+    Stack<Long> st;
+    long min;
     public MinStack() {
         st = new Stack<>();
-        minst = new Stack<>();
+        min = Long.MAX_VALUE;
     }
-
     public void push(int val) {
-        st.push(val);
-        if(minst.size() == 0 || val<minst.peek()) minst.push(val);
-        else minst.push(minst.peek());
+        if(st.size() == 0) min = (long)val;
+        if(val>=min) st.push((long)val);
+        else {
+            st.push((long)val + ((long)val-min));
+            min = val;
+        }   
     }
-
     public void pop() {
+        if(st.peek()<min) min = min+(min-st.peek());
         st.pop();
-        minst.pop();
     }
-
     public int top() {
-        return st.peek();
+        long a = st.peek();
+        if(a<min) return (int)min;
+        else return (int)a;
     }
-
     public int getMin() {
-        return minst.peek();
+        return (int)min;
     }
 }
+
+// class MinStack {
+    // Stack<Integer> st;
+    // Stack<Integer> minst;
+//     public MinStack() {
+        // st = new Stack<>();
+        // minst = new Stack<>();
+//     }
+
+//     public void push(int val) {
+//         st.push(val);
+//         if(minst.size() == 0 || val<minst.peek()) minst.push(val);
+//         else minst.push(minst.peek());
+//     }
+
+//     public void pop() {
+//         st.pop();
+//         minst.pop();
+//     }
+
+//     public int top() {
+//         return st.peek();
+//     }
+
+//     public int getMin() {
+//         return minst.peek();
+//     }
+// }
