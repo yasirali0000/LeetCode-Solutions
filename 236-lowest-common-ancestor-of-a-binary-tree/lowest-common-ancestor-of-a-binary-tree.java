41bfe5ce85278ada8bc3ffd8607dffd9f212c34c
yasirali0000 <yasihkhan33@gmail.com>
2026-09-02 21:04:24 +0530

    Time: 15 ms (90.70%) | Memory: 69.4 MB (92.71%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.java
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.java
@@ -1,25 +1,27 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
+// class Solution {
+//     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+//         if(root == null) return null;
+//         if(root == p || root == q) return root;
+//         boolean pinleft = exist(root.left,p);
+//         boolean qinleft = exist(root.left,q);
+//         if(pinleft && qinleft) return lowestCommonAncestor(root.left,p,q);
+//         else if(!pinleft && !qinleft) return lowestCommonAncestor(root.right,p,q);
+//         else return root;
+//     }
+//     public boolean exist(TreeNode root,TreeNode val) {
+//         if(root == null) return false;
+//         if(root == val) return true;
+//         return exist(root.left,val) || exist(root.right,val);
+//     }
+// }
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         if(root == null) return null;
         if(root == p || root == q) return root;
-        boolean pinleft = exist(root.left,p);
-        boolean qinleft = exist(root.left,q);
-        if(pinleft && qinleft) return lowestCommonAncestor(root.left,p,q);
-        else if(!pinleft && !qinleft) return lowestCommonAncestor(root.right,p,q);
-        else return root;
-    }
-    public boolean exist(TreeNode root,TreeNode val) {
-        if(root == null) return false;
-        if(root == val) return true;
-        return exist(root.left,val) || exist(root.right,val);
+        TreeNode l = lowestCommonAncestor(root.left,p,q);
+        TreeNode r = lowestCommonAncestor(root.right,p,q);
+        if(l!=null && r!=null) return root;
+        if(l==null) return r;
+        else return l;
     }
 }
