1af19ddb38e29441971ac94753443c8d55c2c41b
yasirali0000 <yasihkhan33@gmail.com>
2026-09-03 22:33:58 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (71.16%) - LeetSync

--- 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.java
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.java
@@ -14,18 +14,27 @@
  * }
  */
 class Solution {
+    // public List<Integer> postorderTraversal(TreeNode root) {
+    //     ArrayList<Integer> ans = new ArrayList<>();
+    //     if(root==null) return ans;
+    //     Stack<TreeNode> st = new Stack<>();
+    //     st.push(root);
+    //     while(st.size()>0) {
+    //         TreeNode top = st.pop();
+    //         ans.add(top.val);
+    //         if(top.left!=null) st.push(top.left);
+    //         if(top.right!=null) st.push(top.right);
+    //     }
+    //     Collections.reverse(ans);
+    //     return ans;
+    // }
+    List<Integer> list=new LinkedList<>();
     public List<Integer> postorderTraversal(TreeNode root) {
-        ArrayList<Integer> ans = new ArrayList<>();
-        if(root==null) return ans;
-        Stack<TreeNode> st = new Stack<>();
-        st.push(root);
-        while(st.size()>0) {
-            TreeNode top = st.pop();
-            ans.add(top.val);
-            if(top.left!=null) st.push(top.left);
-            if(top.right!=null) st.push(top.right);
+        if(root!=null){
+            postorderTraversal(root.left);
+            postorderTraversal(root.right);
+            list.add(root.val);
         }
-        Collections.reverse(ans);
-        return ans;
+        return list;
     }
 }
