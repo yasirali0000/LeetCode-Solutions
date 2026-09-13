6380109432af9ac80726ae99b4a245b10bf8bbc2
yasirali0000 <yasihkhan33@gmail.com>
2026-09-13 13:24:06 +0530

    Time: 0 ms (100.00%) | Memory: 47.3 MB (12.78%) - LeetSync

--- 784-insert-into-a-binary-search-tree/insert-into-a-binary-search-tree.java
+++ 784-insert-into-a-binary-search-tree/insert-into-a-binary-search-tree.java
@@ -1,17 +1,24 @@
 class Solution {
+    // public TreeNode insertIntoBST(TreeNode root, int val) {
+    //     if(root==null) root = new TreeNode(val);
+    //     attach(root,val);
+    //     return root;
+    // }
+    // void attach(TreeNode root, int key) {
+    //     if(root.val == key) return;
+    //     if(root.val<key) {
+    //         if(root.right==null) root.right = new TreeNode(key);
+    //         else attach(root.right,key);
+    //     } else {
+    //         if(root.left==null) root.left = new TreeNode(key);
+    //         else attach(root.left,key);
+    //     }
+    // }
     public TreeNode insertIntoBST(TreeNode root, int val) {
-        if(root==null) root = new TreeNode(val);
-        attach(root,val);
+        if(root == null) return new TreeNode(val);
+        if(root.val==val) return root;
+        else if(root.val<val) root.right = insertIntoBST(root.right,val);
+        else root.left = insertIntoBST(root.left,val);
         return root;
-    }
-    void attach(TreeNode root, int key) {
-        if(root.val == key) return;
-        if(root.val<key) {
-            if(root.right==null) root.right = new TreeNode(key);
-            else attach(root.right,key);
-        } else {
-            if(root.left==null) root.left = new TreeNode(key);
-            else attach(root.left,key);
-        }
     }
 }
