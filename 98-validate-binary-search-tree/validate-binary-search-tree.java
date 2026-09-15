51e03c7a351bcb241f9eea44b85d9bde1147739f
yasirali0000 <yasihkhan33@gmail.com>
2026-09-15 20:13:09 +0530

    Time: 1 ms (16.32%) | Memory: 45.2 MB (50.82%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.java
+++ 98-validate-binary-search-tree/validate-binary-search-tree.java
@@ -1,25 +1,59 @@
-class Pair{
-    long max;
-    long min;
-    Pair(long max,long min) {
-        this.max = max;
-        this.min = min;
-    }
-}
+// class Pair {
+//     long max;
+//     long min;
+
+//     Pair(long max, long min) {
+//         this.max = max;
+//         this.min = min;
+//     }
+// }
+
+// class Solution {
+//     static boolean flag;
+
+//     public boolean isValidBST(TreeNode root) {
+//         flag = true;
+//         maxmin(root);
+//         return flag;
+//     }
+
+//     Pair maxmin(TreeNode root) {
+//         if (root == null)
+//             return new Pair(Long.MIN_VALUE, Long.MAX_VALUE);
+//         Pair lst = maxmin(root.left);
+//         Pair rst = maxmin(root.right);
+//         long max = Math.max(root.val, Math.max(lst.max, rst.max));
+//         long min = Math.min(root.val, Math.min(lst.min, rst.min));
+//         if (lst.max >= root.val || rst.min <= root.val)
+//             flag = false;
+//         return new Pair(max, min);
+//     }
+// }
+
 class Solution {
-    static boolean flag;
     public boolean isValidBST(TreeNode root) {
-        flag = true;
-        maxmin(root);
-        return flag;
-    }
-    Pair maxmin(TreeNode root) {
-        if(root==null) return new Pair(Long.MIN_VALUE,Long.MAX_VALUE);
-        Pair lst = maxmin(root.left);
-        Pair rst = maxmin(root.right);
-        long max = Math.max(root.val,Math.max(lst.max,rst.max));
-        long min = Math.min(root.val,Math.min(lst.min,rst.min));
-        if(lst.max>=root.val || rst.min<=root.val) flag = false;
-        return new Pair(max,min);
+        TreeNode curr = root;
+        long prev = Long.MIN_VALUE;;
+        while (curr != null) {
+            if (curr.left != null) {
+                TreeNode pred = curr.left;
+                while (pred.right != null && pred.right != curr)
+                    pred = pred.right;
+                if (pred.right == null) { // link
+                    pred.right = curr;
+                    curr = curr.left;
+                } else { // unlink
+                    pred.right = null;
+                    if(curr.val <= prev) return false;
+                    prev = curr.val;
+                    curr = curr.right;
+                }
+            } else {
+                if(curr.val <= prev) return false;
+                prev = curr.val;
+                curr = curr.right;
+            }
+        }
+        return true;
     }
 }
